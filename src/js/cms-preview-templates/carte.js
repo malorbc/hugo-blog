9e73b1e22f3168123c8dda74860752e4023dd0fe
malorbc <malo.robic@gmail.com>
2022-09-07 12:19:24 +0200

    locale fr

---
+++ src/js/cms-preview-templates/carte.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class CartePreview extends React.Component {
+  render() {
+    return (
+      <div className="ph3 bg-off-white">
+        <div className="center mw6 pv3">
+          <p>Carte ici</p>
+        </div>
+      </div>
+    );
+  }
+}
